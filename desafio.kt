f5d1fecb31e429499a9ddf2101cef43b01f6374b
Adriano Miguel <101981502+AdrianoMiguell@users.noreply.github.com>
2023-10-13 08:26:56 -0300

    Update desafio.kt - teste de adição de formação

--- desafio.kt
+++ desafio.kt
@@ -1,4 +1,7 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
@@ -16,6 +19,37 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+   
+    var cont = arrayOf("Contabilidade", "Matematica", "Engenharia de software")
+    print(cont[0])
+    
+    var formacoes = mutableListOf<Formacao>()
+    var conteudosEduc = mutableListOf<ConteudoEducacional>()
+    var usuarios = mutableListOf<Usuario>()
+    
+	conteudosEduc.add(ConteudoEducacional("Design de software", 10))
+	conteudosEduc.add(ConteudoEducacional("Gestão da informação", 40))
+    conteudosEduc.add(ConteudoEducacional("Engenharia de software", 20))
+    conteudosEduc.add(ConteudoEducacional("Design Gráfico", 180))
+    
+    println(conteudosEduc)
+    
+    val contJuliano = listOf(conteudosEduc.get(1), conteudosEduc.get(2))
+    val formJuliano = Formacao("Juliano", contJuliano)
+	val contAndre = listOf(conteudosEduc.get(0), conteudosEduc.get(2))
+    val formAndre = Formacao("Andre", contAndre)
+
+    val userJuliano = Usuario()
+    println(formJuliano.nome)
+    println(formJuliano.conteudos[0].nome)
+    formacoes.add(formJuliano)
+    formacoes.add(formAndre)
+    
+    print(formacoes)
+    
+//     println(formJuliano)
+    
+//     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+//     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
+
