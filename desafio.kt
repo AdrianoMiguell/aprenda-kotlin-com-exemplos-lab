00d6ed870c27f0701a99da8038dc88c7802b1a6a
Adriano Miguel <101981502+AdrianoMiguell@users.noreply.github.com>
2023-10-13 12:40:13 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -5,51 +5,76 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String, var formacao: Formacao) {
+    fun getNome() : Usuario {
+        println( "\n\nNome do usúario: " + nome)
+        return this
+    } 
+    
+    fun getFormacao() : Usuario {
+        formacao.conteudos.map { println( "\nConteúdo: " + it.nome.toString() + "\nCarga Horária: " + it.duracao.toString() + "\n") }
+        return this
+    }
+    
+    fun getAllData() : Usuario {
+        for(i in 0 .. 10) {
+			print("-----")
+        }
+        getNome()
+        getFormacao()
+        for(i in 0 .. 10) {
+			print("-----")
+        }
+        return this
+    }
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, var nivel: Nivel = Nivel.BASICO)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
-    }
+        	inscritos.add(usuario);
+        	println("\n *** MATRICULA REALIZADA COM SUCESSO!!! *** \n")
+    }    
 }
 
 fun main() {
-   
-    var cont = arrayOf("Contabilidade", "Matematica", "Engenharia de software")
-    print(cont[0])
     
     var formacoes = mutableListOf<Formacao>()
     var conteudosEduc = mutableListOf<ConteudoEducacional>()
     var usuarios = mutableListOf<Usuario>()
     
-	conteudosEduc.add(ConteudoEducacional("Design de software", 10))
-	conteudosEduc.add(ConteudoEducacional("Gestão da informação", 40))
-    conteudosEduc.add(ConteudoEducacional("Engenharia de software", 20))
-    conteudosEduc.add(ConteudoEducacional("Design Gráfico", 180))
+	conteudosEduc.add(ConteudoEducacional("Design de software", 10, Nivel.INTERMEDIARIO))
+	conteudosEduc.add(ConteudoEducacional("Gestão da informação", 40, Nivel.BASICO))
+    conteudosEduc.add(ConteudoEducacional("Engenharia de software", 20, Nivel.DIFICIL))
+    conteudosEduc.add(ConteudoEducacional("Design Gráfico", 180, Nivel.INTERMEDIARIO))
+        
+    val nomesUsers = arrayListOf("Juliano", "Andre", "Adriano")
+    val conteudosUsers = arrayListOf(1, 4, 3)
     
-    println(conteudosEduc)
-    
-    val contJuliano = listOf(conteudosEduc.get(1), conteudosEduc.get(2))
-    val formJuliano = Formacao("Juliano", contJuliano)
-	val contAndre = listOf(conteudosEduc.get(0), conteudosEduc.get(2))
-    val formAndre = Formacao("Andre", contAndre)
+    for(i in 0 .. (nomesUsers.size - 1)) {
+        var conteudosOfUser = mutableListOf<ConteudoEducacional>()
 
-    val userJuliano = Usuario()
-    println(formJuliano.nome)
-    println(formJuliano.conteudos[0].nome)
-    formacoes.add(formJuliano)
-    formacoes.add(formAndre)
+        for (i in 0 .. (conteudosUsers.get(i) - 1)) {
+            conteudosOfUser.add(conteudosEduc.get(i))
+        }
+        
+        var nameOfUser = nomesUsers.get(i)
+        
+        val form = Formacao( nameOfUser, conteudosOfUser)
+		formacoes.add(form)
+    }
     
-    print(formacoes)
+    for(i in 0 .. (formacoes.size - 1)) {
+
+        var user = Usuario(formacoes.get(i).nome, formacoes.get(i))
+		usuarios.add(user)
+        usuarios[i].getAllData()
+    	
+        formacoes[i].matricular(usuarios[i])
+    }
     
-//     println(formJuliano)
-    
-//     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-//     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
-
